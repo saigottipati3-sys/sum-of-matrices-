9851f82a88e831e89dd5898a534d7d5bb4089b88
saigottipati3-sys <saigottipati3@gmail.com>
2026-06-23 17:33:56 +0530

    Add files via upload

---
+++ looping with list.py
@@ -0,0 +1,9 @@
+# List of numbers
+numbers = [5, 12, 18, 7, 20]
+
+# Loop through the list
+for x in numbers:
+    if x > 10:
+        print(f"{x} is a larger number")
+    else:
+        print(f"{x} is a smaller number")
